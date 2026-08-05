23d9067446020c71b7b37fa9556fe4295406d479
AlexandreRimes <alexandrerimesb@gmail.com>
2026-08-05 03:24:06 -0300

    feature: adiciona controllers

--- src/controllers/AddressController.ts
+++ src/controllers/AddressController.ts
@@ -1,4 +1,143 @@
-import { PrismaClient } from "../generated/prisma/client";
+import e, { Request, Response } from "express";
+import { PrismaClient, Prisma } from "../generated/prisma/client";
+import { cp } from "node:fs";
+import auth from "../config/auth"
+import { request } from "node:http";
+import { UserUpdateInput } from "../generated/prisma/models";
 
+const prisma = new PrismaClient({} as any);
 
-const pisma = new PrismaClient()
+class AddressController{
+    
+    public static async createAddress(req: Request, resp: Response){
+
+        try{
+            const{zipCode, street, number, complement, neighborhood, city, state, userId} = req.body;
+
+            const createdAddress = await prisma.address.create({
+                data:{
+
+                    zipCode,
+                    street,
+                    number,
+                    complement,
+                    neighborhood,
+                    city,
+                    state,
+                    userId
+
+                }
+            });
+
+            return resp.status(201).json({
+                message: "Endereço criado",
+                address: createdAddress
+            });
+
+        }
+        catch (error:any){
+            return resp.status(500).json({ message: error.message });
+        }
+
+    }
+
+    public static async readAllAddresses(req: Request, resp: Response){
+        
+        try{
+
+            const addresses = await prisma.address.findMany();
+
+            return resp.status(200).json(addresses);
+
+        }
+        catch (error:any){
+            return resp.status(500).json({message: error.message});
+        }
+
+    }
+
+    public static async readAddress(req: Request, resp: Response){
+
+        try{
+            const {id} = req.params;
+
+            const foundAddress = await prisma.address.findUnique({
+                where:{
+                    id: Number(id)
+                }
+            });
+
+            if(!foundAddress){
+                return resp.status(404).json({message: "Endereço não encontrado"});
+            }
+
+            return resp.status(200).json(foundAddress);
+
+        }
+        catch(error:any){
+            return resp.status(500).json({ message: error.message });
+        }
+
+    }
+
+    public static async updateAddress(req:Request, resp:Response){
+
+        try{
+            const {id} = req.params;
+            const {zipCode, street, number, complement, neighborhood, city, state} = req.body;
+
+            const updatedAddress = await prisma.address.update({
+                where:{
+                    id: Number(id)
+                },
+                data:{
+                    zipCode,
+                    street,
+                    number,
+                    complement,
+                    neighborhood,
+                    city,
+                    state
+                }
+            });
+
+            return resp.status(200).json({
+                message: "Endereço atualizado",
+                address: updatedAddress
+            });
+
+        }
+        catch(error:any){
+            if(error.code === 'P2025'){
+                return resp.status(404).json({ message: "Endereço não encontrado para atualização"});
+            }
+            return resp.status(500).json({ message: error.message});
+        }
+
+    }
+
+    public static async deleteAddress(req:Request, resp:Response){
+
+        try{
+            const {id} = req.params;
+
+            await prisma.address.delete({
+                where:{
+                    id: Number(id)
+                }
+            });
+
+            return resp.status(200).json({
+                message: "Endereço deletado com sucesso"
+            });
+
+        }
+        catch(error:any){
+            if(error.code === 'P2025'){
+                return resp.status(404).json({ message: "Endereço não encontrado para deleção" });
+            }
+            return resp.status(500).json({ message: error.message });
+        }
+
+    }
+}
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,176 @@
-import { PrismaClient } from "../generated/prisma/client";
+import e, { Request, Response } from "express";
+import { PrismaClient, Prisma } from "../generated/prisma/client";
+import { cp } from "node:fs";
+import auth from "../config/auth"
+import { request } from "node:http";
+import { UserUpdateInput } from "../generated/prisma/models";
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({} as any);
 
 class userController{
 
+
+    public static async createUser(req:Request, resp:Response){
+        try{
+
+            const {name,email,cpf,phoneNumber,password} = req.body
+
+            const {hash,salt} = auth.generatePassword(password)
+                const createData = {
+
+                    name,
+                    email,
+                    cpf,
+                    phoneNumber,
+                    hash,
+                    salt,
+                };
+
+            const createdUser = await prisma.user.create({data:createData})
+            return resp.status(201).json({Message:"Usuariario criado com sucesso ",id:createdUser})    
+
+        }
+        catch(error:any){
+            return resp.status(500).json({message:error.message});
+        }
+
+    }
+
+
+
+    public static async readAllUsers(req:Request, resp:Response){
+        try{    
+            const users = await prisma.user.findMany()
+
+            return resp.status(201).json(users);
+
+        }
+        catch(error:any){
+            return resp.status(500).json({message:error.message});
+        }
+    }
+
+
+
+    public static async readUser(req:Request, resp:Response){
+        try{
+            const {id} = req.params
+            const foundUser = await prisma.user.findUnique({
+                where:{
+                    id:String(id)
+                }
+            })
+
+            if(!foundUser){
+                return resp.status(404).json({ message: "Usuário não encontrado" });
+            }
+
+            return resp.status(201).json(foundUser);
+
+        }
+        catch(error:any){
+            return resp.status(500).json({message:error.message});
+        }
+    }
+    
+
+
+    public static async updateUser(req:Request, resp:Response){
+        try{
+            const {userId} = req.params;
+            const {name,email,cpf,phoneNumber,password} = req.body
+
+
+            let passwordData = {};
+
+            if(password){
+                const {hash,salt} = auth.generatePassword(password)
+                passwordData = {hash,salt}
+            }
+
+            
+            const updateInput : UserUpdateInput ={
+                name,
+                email,
+                cpf,
+                phoneNumber,
+                ...passwordData
+            };
+
+            const updatedUser = await prisma.user.update({
+                where:{ 
+                    id:String(userId)
+                },
+
+                data:updateInput,
+                select:{
+                    name:true,
+                    email:true,
+                    cpf:true,
+                    phoneNumber:true,
+                }
+            });
+            return resp.status(200).json({
+            message: "Usuário atualizado com sucesso",
+            user: updatedUser
+        });
+
+        }catch(error:any){
+
+            if (error.code === 'P2025'){
+                return resp.status(404).json({ message: "Usuário não encontrado para deleção" });
+            }
+                return resp.status(404).json({message:error.message});
+        }
+
+    }
+
+    public static async deleteUser(req:Request, resp:Response){
+        
+        try{
+            const {userId} = req.params
+            
+            const deletedUser = await prisma.user.delete({
+                where:{ 
+                    id:String(userId)
+                }
+            });
+        
+            return resp.status(200).json({ 
+            message: "Usuário deletado com sucesso",
+        });
+
+        }catch(error:any){
+            if(error.code === 'P2025'){
+                return resp.status(404).json({ message: "Usuário não encontrado" });
+            }
+                return resp.status(500).json({message:error.message});
+        }
+        
+    }
+
+
+    public static async login(req:Request, resp:Response){
+        try{
+            const {password,email} = req.body
+            const user = await prisma.user.findUnique({
+                where: {email: email} 
+            });
+
+            if(!user){
+                return resp.status(404).json({ message: "Usuário não encontrado" });
+            }
+            if(auth.checkPassword(password,user.hash,user.salt)){
+                const token = auth.generateJWT(Number(user.id));
+                return resp.status(200).json({token:token})
+            }
+            
+            return resp.status(401).json({message:"Senha ou email incorretos"});
+
+        }
+        catch(error:any){
+            return resp.status(500).json({message:error.message})
+        }
+    }
      
 }
